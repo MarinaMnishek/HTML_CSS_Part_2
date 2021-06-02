713f08face8242b1cba976048f49fe8605c765bb
Mnishek <shavyrina.marina@mail.ru>
2021-06-02 16:50:59 +0300

    hometask 3 part 2 (it's not all done yet)

---
+++ JS_Part_2/dz_3/part_2/js/main.js
@@ -0,0 +1,123 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];//массив товаров
+        this.allProducts = [];//массив объектов
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = [...data];
+                this.render()
+            });
+    }
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    // calcSum() {
+    //     return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    // }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+
+
+class ProductItem {
+    constructor(product, img = 'https://computer34.ru/upload/nofoto.jpg') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+let list = new ProductsList();
+
+
+class Cart {
+    constructor(container = '.cart') {
+        this.container = container;
+        this.cartGoods = [];
+        this.getBasket()
+            .then(data => {
+                this.cartGoods = [...data.contents];
+                this.amount = data.amount;
+                this.countGoods = data.countGoods;
+                this.render()
+            })
+
+    }
+    getBasket() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.cartGoods) {
+            const productObj = new CartItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+
+        }
+    }
+
+    // totalPrice() {
+    //     let sum = 0;
+
+    //     for (let product of this.cartGoods) {
+    //         sum += product.price;
+    //     }
+    //     alert(`Стоимость всех товаров: ${sum}`);
+    // }
+}
+
+class CartItem {
+    constructor(product, img = 'https://computer34.ru/upload/nofoto.jpg') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.quantity = product.quantity;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render() {
+        return `<div class="cart-item" data-id="${this.id}">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="cart-item-text">
+                        <h3>${this.title} </h3>
+                        <p>Количество: ${this.quantity} </p>
+                        <p> Цена: ${this.price} $</p>
+                    </div>           
+                </div>`
+
+    }
+}
+
+let cartList = new Cart();
